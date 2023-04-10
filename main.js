20ca76620297613b574d6aec398a962c147807bc
mrbaguette07 <mathsgovlog@gmail.com>
2023-04-10 21:27:32 +0200

    fix(changename): Change jeff to gauthier

--- main.js
+++ main.js
@@ -65,11 +65,11 @@ scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('jeff.png');
+const gauthierTexture = new THREE.TextureLoader().load('gauthier.png');
 
-const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
+const gauthier = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: gauthierTexture }));
 
-scene.add(jeff);
+scene.add(gauthier);
 
 // Moon
 
@@ -89,8 +89,8 @@ scene.add(moon);
 moon.position.z = 30;
 moon.position.setX(-10);
 
-jeff.position.z = -5;
-jeff.position.x = 2;
+gauthier.position.z = -5;
+gauthier.position.x = 2;
 
 // Scroll Animation
 
@@ -100,8 +100,8 @@ function moveCamera() {
   moon.rotation.y += 0.075;
   moon.rotation.z += 0.05;
 
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
+  gauthier.rotation.y += 0.01;
+  gauthier.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
