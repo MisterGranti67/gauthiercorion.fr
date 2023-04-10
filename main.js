dd3720395b8307a3deecd9084b5656f2961d6eab
mrbaguette07 <mathsgovlog@gmail.com>
2023-04-10 21:13:49 +0200

    Exemple en Three JS

---
+++ main.js
@@ -0,0 +1,24 @@
+// Initialiser la scène, la caméra et le rendu
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// Ajouter une sphère à la scène
+var geometry = new THREE.SphereGeometry( 1, 32, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+var sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+// Positionner la caméra
+camera.position.z = 5;
+
+// Animer la sphère
+function animate() {
+	requestAnimationFrame( animate );
+	sphere.rotation.x += 0.01;
+	sphere.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+animate();
